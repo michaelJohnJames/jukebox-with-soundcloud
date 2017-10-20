f9df5be67c8fb043deacc3e76d6e0531e30129da
michaelJohnJames <32465022+michaelJohnJames@users.noreply.github.com>
2017-10-20 13:47:46 -0400

    add song picture

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,3 @@
-var track;
-var trackList;
 (function(ENV) {
     const client_id = ENV.client_id;
     SC.initialize({
@@ -15,14 +13,15 @@ var trackList;
     // find all sounds of buskers licensed under 'creative commons share alike'
 
     var songtext = document.getElementById('songtext');
+    var searchtext = document.getElementById('search');
+    var picture = document.getElementById('pic');
 
     SC.get('/tracks', {
-        q: 'pearl+jam',
+        q: 'songs',
       }).then(function(tracks) {
           let firstTrack = tracks[0];
 
           SC.stream('/tracks/' + firstTrack.id).then(function(player) {
-            track = player;
             player.play();
             //firstTrack.id = songtext.innerHTML;
 
@@ -31,53 +30,111 @@ var trackList;
             var btn2 = document.getElementById('btn2');
             var btn3 = document.getElementById('btn3');
             var btn4 = document.getElementById('btn4');
-            var search = document.getElementById('search')
             var songtext = document.getElementById('songtitle');
             var release = document.getElementById('releasedate');
+            var searchtext = document.getElementById('search');
+            var picture = document.getElementById('pic');
 
 
             songtext.innerHTML = firstTrack.title;
-            //songinfo.innerHTML = firstTrack.purchase_url;
-          //  img.innerHTML = firstTrack.artwork.url;
-            //  release.innerHTML = (firstTrack.release_month + firstTrack.release_day + ', ' + firstTrack.release_year)
+            songinfo.innerHTML = firstTrack.user;
+            songinfo.innerHTML = firstTrack.description;
+            picture.src = firstTrack.artwork_url;
+          //songinfo.innerHTML = firstTrack.permalink.url;
+            //picture.setAttribute("src = img/defaultpicture.jpg") = firstTrack.artwork_url;
 
+            //img.innerHTML = firstTrack.artwork.url;
+          //  release.innerHTML = (firstTrack.release_month + firstTrack.release_day + ', ' + firstTrack.release_year)
 
-            function play() {
-              player.play();
-            };
+          function play() {
+            player.play();
+          };
 
-            function pause() {
-              player.pause();
-            };
+          function pause() {
+            player.pause();
+          };
+
+          function stop() {
+          player.pause();
+          player.seek(0);
+          };
+
 
             btn1.addEventListener('click', play);
             btn2.addEventListener('click', pause);
             btn3.addEventListener('click', stop);
             btn4.addEventListener('click', next);
 
-                function stop() {
-                player.pause();
-             player.seek(0);
-           };
-
-
-
-//          function next() {
-//            SC.get('/tracks', {
-//              q: 'rock',
-//            }).then(function(tracks) {
-//            let secondTrack = tracks[1];
-//             SC.stream('/tracks/' + secondTrack.id).then(function(player) {
-//                player.play();
-
-
-//                btn1.addEventListener('click', play);
-//                btn2.addEventListener('click', pause);
-//                btn3.addEventListener('click', stop);
-//  //              btn4.addEventListener('click', next);
-
 });
 });
+
+function play() {
+  player.play();
+};
+
+function pause() {
+  player.pause();
+};
+
+function stop() {
+player.pause();
+player.seek(0);
+};
+
+
+
+
+function next() { SC.get('/tracks', {
+    q: 'artist',
+  }).then(function(tracks) {
+      let secondTrack = tracks[1];
+
+    SC.stream('/tracks/' + secondTrack.id).then(function(player) {
+        player.play();
+
+
+        var btn1 = document.getElementById('btn1');
+        var btn2 = document.getElementById('btn2');
+        var btn3 = document.getElementById('btn3');
+        var btn4 = document.getElementById('btn4');
+        var search = document.getElementById('search')
+        var songtext = document.getElementById('songtitle');
+        var release = document.getElementById('releasedate');
+        var picture = document.getElementById('pic');
+
+        songtext.innerHTML = secondTrack.title;
+        songinfo.innerHTML = secondTrack.full_name;
+        songinfo.innerHTML = secondTrack.description;
+        picture.src = secondTrack.artwork_url;
+
+        function play() {
+          player.play();
+        };
+
+        function pause() {
+          player.pause();
+        };
+
+        function stop() {
+        player.pause();
+        player.seek(0);
+        };
+
+
+
+
+        btn1.addEventListener('click', play);
+        btn2.addEventListener('click', pause);
+        btn3.addEventListener('click', stop);
+        btn4.addEventListener('click', next);
+
+
+      });
+      });
+}
+
+
+
 
 
             //rest of code goes here

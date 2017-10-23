3ccde725bef74841225710ec02b61a1e55309d13
michaelJohnJames <32465022+michaelJohnJames@users.noreply.github.com>
2017-10-22 21:13:44 -0400

    beautify code

--- public/js/index.js
+++ public/js/index.js
@@ -1,92 +1,93 @@
 (function(ENV) {
-    const client_id = ENV.client_id;
-    SC.initialize({
-      client_id: client_id
-    });
+  const client_id = ENV.client_id;
+  SC.initialize({
+    client_id: client_id
+  });
 
-    var songtext = document.getElementById('songtext');
-    var searchtext = document.getElementById('search');
-    var picture = document.getElementById('pic');
-    var searchbutton = document.getElementById('searchbtn');
-    var songlink = document.getElementById('songlink');
-
-var x = 0
-
-    SC.get('/tracks', {
-         q: 'pearl+jam',
-      }).then(function(tracks) {
+  var songtext = document.getElementById('songtext');
+  var searchtext = document.getElementById('search');
+  var picture = document.getElementById('pic');
+  var searchbutton = document.getElementById('searchbtn');
+  var songlink = document.getElementById('songlink');
+  var x = 0;
 
 
-          var currentTrack = tracks[x];
-
-          SC.stream('/tracks/' + currentTrack.id).then(function(player) {
-            player.play();
-
-            //all event listeners for buttons in here
-            var btn1 = document.getElementById('btn1');
-            var btn2 = document.getElementById('btn2');
-            var btn3 = document.getElementById('btn3');
-            var btn4 = document.getElementById('btn4');
-            var songtext = document.getElementById('songtitle');
-            var release = document.getElementById('releasedate');
-            var searchtext = document.getElementById('search');
-            var picture = document.getElementById('pic');
-
-            songtext.innerHTML = currentTrack.title;
-            songinfo.innerHTML = currentTrack.description;
-            songlink.innerHTML = "Track: " + currentTrack.permalink_url;
-            picture.src = currentTrack.artwork_url;
-
-
-          function play() {
-            player.play();
-          };
-
-          function pause() {
-            player.pause();
-          };
-
-          function stop() {
-          player.pause();
-          player.seek(0);
-          };
-
-
-            btn1.addEventListener('click', play);
-            btn2.addEventListener('click', pause);
-            btn3.addEventListener('click', stop);
-            btn4.addEventListener('click', next);
-            searchbtn.addEventListener('click', playmusic)
-
-
-});
-});
-
-
-function play() {
-  player.play();
-};
-
-function pause() {
-  player.pause();
-};
-
-function stop() {
-player.pause();
-player.seek(0);
-};
-
-
-
-
-function next() { SC.get('/tracks', {
-    q: "pearl+jam"
+  SC.get('/tracks', {
+    q: "songs",
   }).then(function(tracks) {
 
-x++;
- var currentTrack = tracks[x + 1];
+
+    var currentTrack = tracks[x];
 
     SC.stream('/tracks/' + currentTrack.id).then(function(player) {
+      player.play();
+
+      //all event listeners for buttons in here
+      var btn1 = document.getElementById('btn1');
+      var btn2 = document.getElementById('btn2');
+      var btn3 = document.getElementById('btn3');
+      var btn4 = document.getElementById('btn4');
+      var songtext = document.getElementById('songtitle');
+      var release = document.getElementById('releasedate');
+      var searchtext = document.getElementById('search');
+      var picture = document.getElementById('pic');
+
+      songtext.innerHTML = currentTrack.title;
+      songinfo.innerHTML = currentTrack.description;
+      songlink.innerHTML = "Track: " + currentTrack.permalink_url;
+      picture.src = currentTrack.artwork_url;
+
+
+      function play() {
+        player.play();
+      };
+
+      function pause() {
+        player.pause();
+      };
+
+      function stop() {
+        player.pause();
+        player.seek(0);
+      };
+
+
+      btn1.addEventListener('click', play);
+      btn2.addEventListener('click', pause);
+      btn3.addEventListener('click', stop);
+      btn4.addEventListener('click', next);
+      searchbtn.addEventListener('click', playmusic)
+
+
+    });
+  });
+
+
+  function play() {
+    player.play();
+  };
+
+  function pause() {
+    player.pause();
+  };
+
+  function stop() {
+    player.pause();
+    player.seek(0);
+  };
+
+
+
+
+  function next() {
+    SC.get('/tracks', {
+      q: "songs"
+    }).then(function(tracks) {
+
+      x++;
+      var currentTrack = tracks[x + 1];
+
+      SC.stream('/tracks/' + currentTrack.id).then(function(player) {
         player.play();
 
 
@@ -113,8 +114,8 @@ x++;
         };
 
         function stop() {
-        player.pause();
-        player.seek(0);
+          player.pause();
+          player.seek(0);
         };
 
 
@@ -127,14 +128,14 @@ x++;
 
 
       });
-      });
-}
+    });
+  }
 
 
 
 
 
-            //rest of code goes here
+  //rest of code goes here
 
 
-          })  (ENV)
+})(ENV)
